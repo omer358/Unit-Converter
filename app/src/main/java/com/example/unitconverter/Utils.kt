93bc8adfeec35df8e2ad2c801f271ad3320a6bcc
omer358 <omermaki358@gmail.com>
2020-11-03 14:07:11 +0200

    enhancing the list item

--- app/src/main/java/com/example/unitconverter/Utils.kt
+++ app/src/main/java/com/example/unitconverter/Utils.kt
@@ -2,20 +2,20 @@ package com.example.unitconverter
 
 data class Units(
     val unit: String,
-    val icon: Int = R.drawable.ic_baseline_directions_bike_24
+    val icon: Int
 )
 
 fun listOfUnits(): List<Units> {
     return listOf<Units>(
-        Units("Speed"),
-        Units("Mass"),
-        Units("Time"),
-        Units("Temperature"),
-        Units("Distance"),
-        Units("Area"),
-        Units("Data Transfer Rate"),
-        Units("Volume"),
-        Units("Length")
+        Units("Speed", R.drawable.ic_speed),
+        Units("Mass", R.drawable.ic_mass),
+        Units("Time", R.drawable.ic_time),
+        Units("Temperature", R.drawable.ic_termprature_),
+        Units("Distance", R.drawable.ic_speed),
+        Units("Area", R.drawable.ic_area),
+        Units("DTR", R.drawable.ic_internet),
+        Units("Volume", R.drawable.volume),
+        Units("Length", R.drawable.length)
 
     )
 }
